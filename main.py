76dfe4782cf93599994280bb5b2f6b55b66caae6
alexsanjoseph <alexsanjoseph@gmail.com>
2017-10-18 21:05:12 +0530

    Alles ready

--- main.py
+++ main.py
@@ -8,5 +8,10 @@ def main():
         raise UserWarning("Username Invalid")
         exit()
 
-    print("Trying to Buy Streak Freeze")
+    lingo = duolingo.Duolingo('akivalam', "@kivalam@duo")
     lingo.get_streak_info()
+    lingo.get_vocabulary()
+
+    lingo.buy_streak_freeze()
+    print("Trying to Buy Streak Freeze")
+    lingo.get_user_info()
