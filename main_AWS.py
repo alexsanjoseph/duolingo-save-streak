a2616317ac737e78521f1c62c76bacb923493391
alexsanjoseph <alexsanjoseph@gmail.com>
2017-10-22 10:18:41 +0530

    CLeaner code

--- main_AWS.py
+++ main_AWS.py
@@ -1,3 +1,23 @@
+def process_single_user(username, password):
+    try:
+        lingo = duolingo.Duolingo(username, password)
+    except ValueError:
+        raise Exception("Username Invalid")
+
+    print("Trying to Buy Streak Freeze for " + username)
+    if(lingo.buy_streak_freeze()):
+        print("Bought streak freeze for " + username)
+    else:
+        print("Unable to buy streak freeze")
+
+    try:
+        print("Trying to Buy Double or nothing for " + username)
+        lingo.buy_item('rupee_wager', 'en')
+        print("Bought Double or nothing for " + username)
+    except:
+        print("Unable to buy double or nothing")
+
+
 
 def main(a, b):
     import duolingo, os
@@ -5,17 +25,6 @@ def main(a, b):
     usernames = os.environ['usernames'].split(',')
     passwords = os.environ['passwords'].split(',')
 
-    for username,password in zip(usernames,passwords):
-        try:
-            lingo = duolingo.Duolingo(username, password)
-        except ValueError:
-            raise Exception("Username Invalid")
+    list(map(process_single_user, usernames, passwords))
 
-        print("Trying to Buy Streak Freeze for " + username)
-        print(lingo.buy_streak_freeze())
-
-        try:
-            print("Trying to Buy Double or nothing for " + username)
-            lingo.buy_item('rupee_wager', 'en')
-        except:
-            raise UserWarning("Unable to buy double or nothing")
+main(1, 1)
