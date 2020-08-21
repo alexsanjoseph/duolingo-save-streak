265b9aa6a69d4a46fa1b126429972c2ecea5812c
alexsanjoseph <alexsanjoseph@gmail.com>
2020-08-22 00:24:15 +0530

    streak freeze count

--- main_AWS.py
+++ main_AWS.py
@@ -1,3 +1,7 @@
+def streak_freeze_count(lingo):
+    return lingo.__dict__['user_data'].__dict__['tracking_properties']['num_item_streak_freeze']
+
+
 def process_single_user(username, password):
     import duolingo
     try:
@@ -8,6 +12,8 @@ def process_single_user(username, password):
     stuff_to_purchase = ['streak_freeze', 'rupee_wager']
 
     for item in stuff_to_purchase:
+        if(item == 'streak_freeze' and streak_freeze_count(lingo) >= 3):
+            continue
         try:
             print("Trying to Buy " + item + " for " + username)
             lingo.buy_item(item, 'en')
@@ -19,14 +25,14 @@ def process_single_user(username, password):
 
 
 
-def main(a, b):
-    import duolingo, os
+def main():
+    import os
 
     usernames = os.environ['usernames'].split(',')
     passwords = os.environ['passwords'].split(',')
 
     list(map(process_single_user, usernames, passwords))
 
-import yaml
-learning_config = yaml.safe_load(open("../learning_config/config.yaml"))
-process_single_user(learning_config[0]['username'], learning_config[0]['password'])
+
+if __name__ == "__main__":
+    main()
